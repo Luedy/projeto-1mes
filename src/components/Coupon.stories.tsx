09997e0a5ce23092b91fb2c07e6556ec0f6c9969
Lucas Luedy <luedyrego@gmail.com>
2022-12-11 18:00:10 -0300

    cupon

---
+++ src/components/Coupon.stories.tsx
@@ -0,0 +1,17 @@
+import {CouponProps,CouponWrapper} from './Coupon';
+import {Meta,StoryObj} from '@storybook/react'
+
+export default{
+  title:'Components/Coupon',
+  component:CouponWrapper,
+  args:{
+    
+  },
+  argTypes:{
+
+  }
+}as Meta<CouponProps>
+
+export const Coupon: StoryObj<CouponProps> = {
+  args:{ }
+}
