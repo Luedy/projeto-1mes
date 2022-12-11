33cadde5b58fc6745cb53a877b7fcaee7f7fbf09
Lucas Luedy <luedyrego@gmail.com>
2022-12-11 18:10:35 -0300

    ajuste no Product

--- src/routers.tsx
+++ src/routers.tsx
@@ -3,6 +3,7 @@ import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 
 import { HomeWeb } from "./screen/HomeWeb/HomeWeb"; "./screen/HomeWeb/HomeWeb";
 import { Category } from "./screen/Category";
+
 import { Product } from "./screen/Product-Page/Product"
 export default function Routess(){
   return(
@@ -11,6 +12,7 @@ export default function Routess(){
         <Route path="/" element={<HomeWeb/>} />
         <Route path="/HomeCategory" element={<Category/>} />
         <Route path="/HomeCategory/Label" element={<Product/>} />
+       
       </Routes>
     </Router>
   )
